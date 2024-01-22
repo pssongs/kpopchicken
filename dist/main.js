fc8ef9efdfde1836da0277bfddf1d10a65af67df
Paul Song <jaebinpsong@gmail.com>
2024-01-22 02:56:22 -0800

    made responsive and now waiting to upload menu

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{document.getElementById("loading");const e=document.querySelector(".hamburger"),t=document.querySelector(".nav-menu");e.addEventListener("click",(function(){e.classList.toggle("active"),t.classList.toggle("active")}))})();
